f5daa38b1e87d5bdccbe4a140de8adf1786e53e0
Datshin1511 <datshinr2002@gmail.com>
2025-03-07 14:04:45 +0530

    Base commit

---
+++ src/PointSET.java
@@ -0,0 +1,64 @@
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+    public PointSET() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void insert(Point2D p) {
+
+    }
+
+    public boolean contains(Point2D p) {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        return new Iterable<Point2D>() {
+
+            @Override
+            public Iterator<Point2D> iterator() {
+                // TODO Auto-generated method stub
+                return new Iterator<Point2D>() {
+
+                    @Override
+                    public boolean hasNext() {
+                        // TODO Auto-generated method stub
+                        throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+                    }
+
+                    @Override
+                    public Point2D next() {
+                        // TODO Auto-generated method stub
+                        throw new UnsupportedOperationException("Unimplemented method 'next'");
+                    }
+                    
+                };
+            }
+            
+        };
+    }
+
+    public Point2D nearest(Point2D p) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}   
