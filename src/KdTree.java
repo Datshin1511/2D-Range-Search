73cf4c0cce745be01c307eafcece5e72d3574c77
Datshin1511 <datshinr2002@gmail.com>
2025-03-09 20:04:05 +0530

    Supporting classes

--- src/KdTree.java
+++ src/KdTree.java
@@ -1,3 +1,66 @@
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
 public class KdTree {
-    
+
+    public KdTree() {
+
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public int size() {
+
+    }
+
+    public void insert(Point2D p) {
+
+    }
+
+    public boolean contains(Point2D p) {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        return new Iterable<Point2D>() {
+
+            @Override
+            public Iterator<Point2D> iterator() {
+                // TODO Auto-generated method stub
+                return new Iterator<Point2D>() {
+
+                    @Override
+                    public boolean hasNext() {
+                        // TODO Auto-generated method stub
+                        throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+                    }
+
+                    @Override
+                    public Point2D next() {
+                        // TODO Auto-generated method stub
+                        throw new UnsupportedOperationException("Unimplemented method 'next'");
+                    }
+                    
+                };
+            }
+            
+        };
+    }
+
+    public Point2D nearest(Point2D p) {
+
+    }
+
+    // MAIN - UNIT TESTING
+    public static void main(String args[]) {
+
+    }
 }
